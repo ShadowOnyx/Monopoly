34a99890ddc6095df0ed2f956118dda62d2e1a91
ShadowOnyx <Jonatan.Oliveira@MU-C1-18.mullen.pvt.k12.co.us>
2015-12-01 07:47:30 -0700

    Added GO +$200

--- Monopoly/src/Players.java
+++ Monopoly/src/Players.java
@@ -5,23 +5,32 @@ public abstract class Players
 	protected int money;
 	public static int rollDice()
 		{
-		int die1 = (int)(Math.random()*5)+1;	
-		int die2 = (int)(Math.random()*5)+1;
+		int die1 = (int)(Math.random()*6)+1;
+		int die2 = (int)(Math.random()*6)+1;
 		int dieRoll = die1+die2;
 		return dieRoll;
 		}
-
+	
+	
+	
 	public static int doTurn(int r, int p)
 		{
+		if(r+p>40)
+			{
+			System.out.println("You passed GO, collect 200");
+			Runner.players.get(0).setMoney(Runner.players.get(0).getMoney()+200);	
+			}
 		int newPosition = (r+p)%40;
 		return newPosition;
 		}
-	public int getCurrentLocation() {
+	public int getCurrentLocation() 
+		{
 		return currentLocation;
-	}
-	public void setCurrentLocation(int currentLocation) {
+		}
+	public void setCurrentLocation(int currentLocation) 
+		{
 		this.currentLocation = currentLocation;
-	}
+		}
 	public int getMoney()
 		{
 			return money;
