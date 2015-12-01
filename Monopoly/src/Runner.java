0232679adf8106e8f9db040ed67ab2093356136b
ShadowOnyx <Jonatan.Oliveira@MU-C1-18.mullen.pvt.k12.co.us>
2015-12-01 09:02:11 -0700

    Added Star Wars Theme

--- Monopoly/src/Runner.java
+++ Monopoly/src/Runner.java
@@ -22,7 +22,7 @@ public class Runner
 				System.out.println("You rolled a "+dieRoll);
 				System.out.println("You landed on "+board.get(currentPosition).getName());
 				if(ownedByPlayer.size()>0)
-					{
+					{  
 					System.out.println("Inventory:");
 					displayInventory();
 					}
@@ -67,45 +67,45 @@ public class Runner
 			{
 			players.add(new Human(0,1500));
 			board.add(new NonBuyable(0,"GO",false));
-			board.add(new Colored(1,"Medditerranean Avenue", "Brown",60,false,true));
-			board.add(new NonBuyable(2,"Community Chest",false));
-			board.add(new Colored(3,"Baltic Avenue", "Brown",60,false,true));
-			board.add(new NonBuyable(4,"Income Tax",false));
-			board.add(new NonColored(5,"Reading Railroad",200,false,true));
-			board.add(new Colored(6,"Oriental Avenue", "Blue",100,false,true));
-			board.add(new NonBuyable(7,"Chance",false));
-			board.add(new Colored(8,"Vermont Avenue", "Blue",100,false,true));
-			board.add(new Colored(9,"Connecticut Avenue", "Blue",120,false,true));
-			board.add(new NonBuyable(10,"Jail",false));
-			board.add(new Colored(11,"St. Charles Place", "Pink",140,false,true));
-			board.add(new NonColored(12,"Electric Company",150,false,true));
-			board.add(new Colored(13,"States Avenue", "Pink",140,false,true));
-			board.add(new Colored(14,"Virginia Avenue", "Pink",160,false,true));
-			board.add(new NonColored(15,"Pennsylvania Railroad",200,false,true));
-			board.add(new Colored(16,"St. James Place", "Orange",180,false,true));
-			board.add(new NonBuyable(17,"Community Chest",false));
-			board.add(new Colored(18,"Tennessee Avenue", "Orange",180,false,true));
-			board.add(new Colored(19,"New York Avenue", "Orange",200,false,true));
-			board.add(new NonBuyable(20,"Free Parking",false));
-			board.add(new Colored(21,"Kentucky Avenue", "Red",220,false,true));
-			board.add(new NonBuyable(22,"Chance",false));
-			board.add(new Colored(23,"Indiana Avenue", "Red",220,false,true));
-			board.add(new Colored(24,"Illinois Avenue", "Red",240,false,true));		
-			board.add(new NonColored(25,"B & O Railroad",200,false,true));		
-			board.add(new Colored(26,"Atlantic Avenue", "Yellow",260,false,true));		
-			board.add(new Colored(27,"Ventnor Avenue", "Yellow",260,false,true));		
-			board.add(new NonColored(28,"Water Works",150,false,true));		
-			board.add(new Colored(29,"Marian Gardens", "Yellow",280,false,true));		
-			board.add(new NonBuyable(30,"Go To Jail",false));		
-			board.add(new Colored(31,"Pacific Avenue", "Green",300,false,true));		
-			board.add(new Colored(32,"North Carolina Avenue", "Green",300,false,true));
-			board.add(new NonBuyable(33,"Community Chest",false));
-			board.add(new Colored(34,"Pennsylvania Avenue", "Green",320,false,true));
-			board.add(new NonColored(35,"Short Line",200,false,true));
-			board.add(new NonBuyable(36,"Chance",false));
-			board.add(new Colored(37,"Park Place", "Blue",350,false,true));
-			board.add(new NonBuyable(38,"Luxury Tax",false));
-			board.add(new Colored(39,"Boardwalk", "Blue",400,false,true));
+			board.add(new Colored(1,"Dagobah - Swamp", "Purple",60,false,true));
+			board.add(new NonBuyable(2,"Rebel Scum",false));
+			board.add(new Colored(3,"Dagobah  - Yodas Hut", "Purple",60,false,true));
+			board.add(new NonBuyable(4,"Docking Tax",false));
+			board.add(new NonColored(5,"Tie Fighter",200,false,true));
+			board.add(new Colored(6,"Hoth - Echo Base", "Blue",100,false,true));
+			board.add(new NonBuyable(7,"Imperial Chance",false));
+			board.add(new Colored(8,"Hoth - Frozen Plains", "Blue",100,false,true));
+			board.add(new Colored(9,"Hoth - North Ridge", "Blue",120,false,true));
+			board.add(new NonBuyable(10,"CARBON JAIL",false));
+			board.add(new Colored(11,"Tatooine - Lars Homestead", "Pink",140,false,true));
+			board.add(new NonColored(12,"Reactor Core",150,false,true));
+			board.add(new Colored(13,"Tatooine - Mos Eisly ", "Pink",140,false,true));
+			board.add(new Colored(14,"Tatooine - Yodas Second Swamp", "Pink",160,false,true));
+			board.add(new NonColored(15,"Millennium Falcon",200,false,true));
+			board.add(new Colored(16,"Yavin Four - War Room", "Orange",180,false,true));
+			board.add(new NonBuyable(17,"Rebel Scum",false));
+			board.add(new Colored(18,"Yavin Four - Massassi Temple", "Orange",180,false,true));
+			board.add(new Colored(19,"Yavin Temple - Temple Throne Room", "Orange",200,false,true));
+			board.add(new NonBuyable(20,"Free Fighter Parking",false));
+			board.add(new Colored(21,"Cloud City - Landos Platform", "Red",220,false,true));
+			board.add(new NonBuyable(22,"Imperial Chance",false));
+			board.add(new Colored(23,"Cloud City - Carbon Freezing Chamber", "Red",220,false,true));
+			board.add(new Colored(24,"Cloud City - Reactor Control Room", "Red",240,false,true));		
+			board.add(new NonColored(25,"X-Wing Fighter",200,false,true));		
+			board.add(new Colored(26,"Death Star - Landing Bay", "Yellow",260,false,true));		
+			board.add(new Colored(27,"Death Star - Throne Room", "Yellow",260,false,true));		
+			board.add(new NonColored(28,"Moisture Farm",150,false,true));		
+			board.add(new Colored(29,"Death Star - Central Core", "Yellow",280,false,true));		
+			board.add(new NonBuyable(30,"IT'S A TRAP",false));		
+			board.add(new Colored(31,"Endor - Forest", "Green",300,false,true));		
+			board.add(new Colored(32,"Endor - Shield Generator", "Green",300,false,true));
+			board.add(new NonBuyable(33,"Rebel Scum",false));
+			board.add(new Colored(34,"Endor - Ewok Village", "Green",320,false,true));
+			board.add(new NonColored(35,"Star Destroyer",200,false,true));
+			board.add(new NonBuyable(36,"Imperial Chance",false));
+			board.add(new Colored(37,"Coruscant - Monument Square", "Blue",350,false,true));
+			board.add(new NonBuyable(38,"Bounty",false));
+			board.add(new Colored(39,"Coruscant - Imperial Palace", "Blue",400,false,true));
 			
 			
 			
